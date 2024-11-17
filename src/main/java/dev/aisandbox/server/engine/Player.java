80111b1f2d3a491814dc498ec22451e22b4d9cb7
Graham Evans <graham.evans@maersk.com>
2024-11-17 19:24:16 +0000

    store player name

--- src/main/java/dev/aisandbox/server/engine/Player.java
+++ src/main/java/dev/aisandbox/server/engine/Player.java
@@ -3,9 +3,8 @@ package dev.aisandbox.server.engine;
 import com.google.protobuf.GeneratedMessage;
 
 public interface Player {
+    String getPlayerName();
     void send(GeneratedMessage o);
-
     GeneratedMessage receive(GeneratedMessage state);
-
     void close();
 }
