3c75f0318a50c5b6e79826732a8aa060739ed4c1
Graham Evans <graham.evans@maersk.com>
2024-11-23 22:28:19 +0000

    base scenarios

---
+++ src/main/java/dev/aisandbox/server/simulation/coingame/CoinScenario.java
@@ -0,0 +1,18 @@
+package dev.aisandbox.server.simulation.coingame;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public enum CoinScenario {
+    SINGLE_21_2(new int[]{21}, 2),
+    SINGLE_21_3(new int[]{21}, 3),
+    DOUBLE_21_2(new int[]{21}, 2),
+    DOUBLE_21_3(new int[]{21}, 3),
+    NIM(new int[]{1,3,5,7},7);
+
+    private final int[] rows;
+    private final int max;
+}
