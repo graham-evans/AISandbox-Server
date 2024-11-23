841fb433224b5f336595db9cecabbcaed47e771e
Graham Evans <graham.evans@maersk.com>
2024-11-23 18:49:30 +0000

    add colour values to theme

--- src/main/java/dev/aisandbox/server/engine/Theme.java
+++ src/main/java/dev/aisandbox/server/engine/Theme.java
@@ -1,20 +1,26 @@
 package dev.aisandbox.server.engine;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.awt.*;
 
 @Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum Theme {
 
-    DEFAULT(Color.WHITE,Color.BLACK);
+    DEFAULT(
+            Color.decode("#eeeeee"),
+            Color.BLACK,
+            Color.lightGray,
+            Color.darkGray
+    );
 
     private final Color background;
-    private final Color foreground;
+    private final Color text;
+    private final Color widgetBackground;
+    private final Color graphBackground;
 
-    Theme(Color background, Color foreground) {
-        this.background = background;
-        this.foreground = foreground;
-    }
 
 }
