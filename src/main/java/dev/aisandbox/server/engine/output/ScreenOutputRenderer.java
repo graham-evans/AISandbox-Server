d2e54dce50a7c8d7729cff3a72dd26d9f17fc62f
Graham Evans <gde@pobox.com>
2024-12-27 15:52:50 +0000

    simple (single buffered) scaled image written to canvas.

--- src/main/java/dev/aisandbox/server/engine/output/ScreenOutputRenderer.java
+++ src/main/java/dev/aisandbox/server/engine/output/ScreenOutputRenderer.java
@@ -13,9 +13,7 @@ import java.awt.image.BufferedImage;
 public class ScreenOutputRenderer implements OutputRenderer {
     private final Simulation simulation;
 
-    JFrame frame = null;
-    JLabel label = null;
-    long frameCounter = 1;
+    ScreenFrame screenFrame=null;
 
     @Override
     public String getName() {
@@ -29,23 +27,11 @@ public class ScreenOutputRenderer implements OutputRenderer {
         Graphics2D g2d = image.createGraphics();
         simulation.visualise(g2d);
         // setup screen
-        if (frame == null) {
-            // setup label
-            label = new JLabel();
-            label.setIcon(new ImageIcon(image));
-            label.setText("Frame " + frameCounter);
-            label.setVerticalTextPosition(SwingConstants.BOTTOM);
-            label.setHorizontalTextPosition(SwingConstants.CENTER);
-            frameCounter++;
-            // setup screen
-            frame = new JFrame("Simulation Output");
-            frame.add(label);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(800, 600);
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
-            frame.toFront();
+        if (screenFrame == null) {
+            screenFrame = new ScreenFrame();
         }
+        // push image
+        screenFrame.updateImage(image);
     }
 
     @Override
@@ -53,12 +39,6 @@ public class ScreenOutputRenderer implements OutputRenderer {
         // close window
     }
 
-    public class MyCanvas extends Canvas {
-        public void paint(Graphics g) {
-            Toolkit t = Toolkit.getDefaultToolkit();
-            Image i = t.getImage("p3.gif");
-            g.drawImage(i, 120, 100, this);
-        }
-    }
+
 
 }
