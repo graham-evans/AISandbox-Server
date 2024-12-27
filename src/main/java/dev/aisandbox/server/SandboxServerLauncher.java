17a7f5f8ceb10fb311d58838c29ac57006ee1fdc
Graham Evans <gde@pobox.com>
2024-12-27 15:52:49 +0000

    use reflection to list options

--- src/main/java/dev/aisandbox/server/SandboxServerLauncher.java
+++ src/main/java/dev/aisandbox/server/SandboxServerLauncher.java
@@ -6,10 +6,15 @@ import dev.aisandbox.server.options.RuntimeUtils;
 import dev.aisandbox.server.simulation.highlowcards.HighLowCardsBuilder;
 import dev.aisandbox.server.simulation.highlowcards.proto.ClientAction;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.cli.HelpFormatter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Component
@@ -21,6 +26,7 @@ public class SandboxServerLauncher implements CommandLineRunner {
     public void run(String... args) throws Exception {
         // parse the command line
         RuntimeOptions runtimeOptions = RuntimeUtils.parseCommandLine(args);
+        // decide which of the main three actions to perform
         switch (runtimeOptions.command()) {
             case HELP -> help(runtimeOptions);
             case RUN -> runSimulation(runtimeOptions);
@@ -33,7 +39,9 @@ public class SandboxServerLauncher implements CommandLineRunner {
         if (runtimeOptions.simulation() != null) {
             helpSimulation(runtimeOptions.simulation());
         } else {
-            System.out.println("Usage: java -jar sandbox-server.jar [options]");
+            // automatically generate the help statement
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("java -jar sandbox-server.jar", RuntimeUtils.getOptions());
         }
     }
 
@@ -65,14 +73,49 @@ public class SandboxServerLauncher implements CommandLineRunner {
     }
 
     private void listOptions(RuntimeOptions options) {
-        if (options.simulation() == null) {
-            System.out.println("Simulation name: " + options.simulation());
+        Optional<SimulationBuilder> oBuilder = findBuilder(options.simulation());
+        if (oBuilder.isPresent()) {
+            // show the options for the simulation
+            SimulationBuilder sim = oBuilder.get();
+            System.out.println(sim.getName());
+            System.out.println("Minimum # clients "+sim.getMinPlayerCount());
+            System.out.println("Maximum # clients "+sim.getMaxPlayerCount());
+            System.out.println("Options:");
+            for (Method method:sim.getClass().getDeclaredMethods()) {
+                if (method.getName().startsWith("set") && method.getParameterCount() == 1) {
+                    System.out.print(" ");
+                    System.out.print(method.getName().substring(3));
+                    System.out.print(":");
+                    System.out.println(method.getParameters()[0].getType().getName());
+                }
+            }
         } else {
-            System.out.println("listing simulations:");
+            // list the available simulations
+            System.out.println("Available simulations:");
             for (SimulationBuilder simulationBuilder : simulationBuilders) {
-                System.out.println(simulationBuilder.getName());
+                System.out.print(simulationBuilder.getName());
+                System.out.print(" (");
+                System.out.print(simulationBuilder.getMinPlayerCount());
+                if (simulationBuilder.getMinPlayerCount() == simulationBuilder.getMaxPlayerCount()) {
+                    System.out.println(simulationBuilder.getMinPlayerCount() == 1 ? " client)" : " clients)");
+                } else {
+                    System.out.print(" to ");
+                    System.out.print(simulationBuilder.getMaxPlayerCount());
+                    System.out.println(" clients)");
+                }
             }
         }
     }
+
+    private Optional<SimulationBuilder> findBuilder(String name) {
+        Optional<SimulationBuilder> result = Optional.empty();
+        for (SimulationBuilder simulationBuilder : simulationBuilders) {
+            if (simulationBuilder.getName().equalsIgnoreCase(name)) {
+                result = Optional.of(simulationBuilder);
+            }
+        }
+        return result;
+    }
+
 
 }
