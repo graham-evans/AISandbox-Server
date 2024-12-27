f0f03636824e0f27dc1337f72290b93fae64c603
Graham Evans <gde@pobox.com>
2024-12-27 15:52:54 +0000

    move logo size constants

--- src/main/java/dev/aisandbox/server/engine/output/OutputConstants.java
+++ src/main/java/dev/aisandbox/server/engine/output/OutputConstants.java
@@ -7,5 +7,6 @@ public class OutputConstants {
 
     public final static int HD_WIDTH = 1920;
     public final static int HD_HEIGHT = 1080;
-
+    public static final int LOGO_WIDTH=91;
+    public static final int LOGO_HEIGHT=108;
 }
