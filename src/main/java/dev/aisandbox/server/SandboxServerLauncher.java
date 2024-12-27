e0a371e72a84b2e3b78f785a6d7106712a0f4ff3
Graham Evans <gde@pobox.com>
2024-12-27 15:52:48 +0000

    test client for high low game

--- src/main/java/dev/aisandbox/server/SandboxServerLauncher.java
+++ src/main/java/dev/aisandbox/server/SandboxServerLauncher.java
@@ -1,14 +1,11 @@
 package dev.aisandbox.server;
 
-import dev.aisandbox.server.engine.Player;
-import dev.aisandbox.server.engine.Simulation;
-import dev.aisandbox.server.engine.SimulationInfo;
-import dev.aisandbox.server.simulation.highlowcards.HighLowCards;
-import dev.aisandbox.server.simulation.highlowcards.proto.Person;
+import dev.aisandbox.server.engine.*;
+import dev.aisandbox.server.simulation.highlowcards.HighLowCardsBuilder;
+import dev.aisandbox.server.simulation.highlowcards.proto.ClientAction;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -18,19 +15,19 @@ public class SandboxServerLauncher implements CommandLineRunner {
     public void run(String... args) throws Exception {
         System.out.println("Hello World!");
         // create simulation
-        SimulationInfo simulationInfo = new HighLowCards();
+        SimulationBuilder simulationBuilder = new HighLowCardsBuilder();
         // create players
-        List<Player> players = new ArrayList<>();
-        for (int i=0;i<simulationInfo.getPlayerCount();i++) {
-            players.add(new Player());
-        }
-        // run simulation
-        Simulation sim = simulationInfo.createSimulation(players);
+        List<Player> players = List.of(new Player("Player",9000, ClientAction.class));
+        // create simulation
+        Simulation sim = simulationBuilder.build(players);
+        // create output
+        OutputRenderer out = new NullOutputRenderer();
         // perform 10 steps
-        for (int i=0;i<10;i++) {
-            sim.step();
+        for (int i = 0; i < 10; i++) {
+            sim.step(out);
         }
-        // create proto
-        Person person = Person.newBuilder().setName("name").build();
+        // finish simulation
+        sim.close();
+        players.forEach(Player::close);
     }
 }
