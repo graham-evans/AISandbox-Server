ae1cf309733155eb9573f905b79c3217c3eaa2c9
Graham Evans <gde@pobox.com>
2024-12-27 15:52:54 +0000

    add second row

--- src/main/java/dev/aisandbox/server/simulation/coingame/CoinScenario.java
+++ src/main/java/dev/aisandbox/server/simulation/coingame/CoinScenario.java
@@ -9,8 +9,8 @@ import lombok.RequiredArgsConstructor;
 public enum CoinScenario {
     SINGLE_21_2(new int[]{21}, 2),
     SINGLE_21_3(new int[]{21}, 3),
-    DOUBLE_21_2(new int[]{21}, 2),
-    DOUBLE_21_3(new int[]{21}, 3),
+    DOUBLE_21_2(new int[]{21,21}, 2),
+    DOUBLE_21_3(new int[]{21,21}, 3),
     NIM(new int[]{1,3,5,7},7);
 
     @Getter
