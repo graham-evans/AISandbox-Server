25cbbcf6c97825401a27ff1a2056d4c0f22d4063
Graham Evans <gde@pobox.com>
2024-12-27 15:52:53 +0000

    Use new text widgets

--- src/main/java/dev/aisandbox/server/simulation/highlowcards/HighLowCards.java
+++ src/main/java/dev/aisandbox/server/simulation/highlowcards/HighLowCards.java
@@ -2,11 +2,12 @@ package dev.aisandbox.server.simulation.highlowcards;
 
 import dev.aisandbox.server.engine.Player;
 import dev.aisandbox.server.engine.Simulation;
-import dev.aisandbox.server.engine.chart.RollingHistogramChart;
-import dev.aisandbox.server.engine.chart.RollingScoreChart;
-import dev.aisandbox.server.engine.chart.TextChart;
+import dev.aisandbox.server.engine.Theme;
 import dev.aisandbox.server.engine.output.OutputConstants;
 import dev.aisandbox.server.engine.output.OutputRenderer;
+import dev.aisandbox.server.engine.widget.RollingHistogramChart;
+import dev.aisandbox.server.engine.widget.RollingScoreChart;
+import dev.aisandbox.server.engine.widget.TextWidget;
 import dev.aisandbox.server.simulation.common.Card;
 import dev.aisandbox.server.simulation.common.Deck;
 import dev.aisandbox.server.simulation.highlowcards.proto.ClientAction;
@@ -21,14 +22,16 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.List;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class HighLowCards implements Simulation {
 
     private static final int MARGIN = 80;
-    private static final int GRAPH_WIDTH = (OutputConstants.HD_WIDTH - 3 * MARGIN) / 2;
-    private static final int GRAPH_HEIGHT = 300;
-    private static final int TEXT_HEIGHT = 200;
+    private static final int TEXT_HEIGHT = 280;
+    private static final int TEXT_WIDTH = OutputConstants.HD_WIDTH - 3 * MARGIN - 920;
+    private static final int GRAPH_WIDTH = 920;
+    private static final int GRAPH_HEIGHT = (OutputConstants.HD_HEIGHT - TEXT_HEIGHT - MARGIN * 4) / 2;
     private final Player player;
     private final int cardCount;
     private final Random rand = new Random();
@@ -37,11 +40,14 @@ public class HighLowCards implements Simulation {
     private final Map<String, BufferedImage> cardImages = new HashMap<>();
     private final RollingScoreChart rollingScoreChart;
     private final RollingHistogramChart rollingHistogramChart;
-    private final TextChart textChart;
+    private final TextWidget textWidget;
+    private final TextWidget summaryChart;
+    private final Theme theme;
 
-    public HighLowCards(Player player, int cardCount) {
+    public HighLowCards(Player player, int cardCount, Theme theme) {
         this.player = player;
         this.cardCount = cardCount;
+        this.theme = theme;
         rollingScoreChart = RollingScoreChart.builder()
                 .dataWindow(200)
                 .width(GRAPH_WIDTH)
@@ -54,7 +60,8 @@ public class HighLowCards implements Simulation {
                 .height(GRAPH_HEIGHT)
                 .cache(true)
                 .build();
-        textChart = TextChart.builder().width(OutputConstants.HD_WIDTH - 2 * MARGIN).height(TEXT_HEIGHT).build();
+        textWidget = new TextWidget(TEXT_WIDTH, TEXT_HEIGHT, 14, "Arel", theme);
+        summaryChart = new TextWidget(TEXT_WIDTH, GRAPH_HEIGHT * 2 + MARGIN, 20, "Ariel", theme);
         reset();
     }
 
@@ -89,7 +96,7 @@ public class HighLowCards implements Simulation {
         Card previousCard = faceUpCards.getLast();
         Card nextCard = faceDownCards.getFirst();
         // render the current frame
-        textChart.addText("Showing " + faceUpCards.getLast().getShortDrescription());
+        textWidget.addText("Showing " + faceUpCards.stream().map(Card::getShortDrescription).collect(Collectors.joining(",")));
         output.display();
         // send the current state and request an action
         ClientAction action = player.recieve(getPlayState(Signal.PLAY, faceDownCards.size()), ClientAction.class);
@@ -102,20 +109,22 @@ public class HighLowCards implements Simulation {
                         ||
                         (action.getAction() == HighLowChoice.HIGH && nextCard.cardValue().getValueAceHigh() > previousCard.cardValue().getValueAceHigh())) {
             // correct guess
-            textChart.addText("[" + player.getPlayerName() + "] " + action.getAction().name() + " - correct");
+            textWidget.addText("[" + player.getPlayerName() + "] " + action.getAction().name() + " - correct");
             output.display();
             // reset if we're finished.
             if (faceDownCards.isEmpty()) {
                 rollingScoreChart.addScore(faceUpCards.size());
                 rollingHistogramChart.addScore(faceUpCards.size());
+                summaryChart.addText("Median=3");
                 player.send(getPlayState(Signal.RESET, faceUpCards.size()));
                 reset();
             }
         } else {
             // incorrect guess - game over
-            textChart.addText("[" + player.getPlayerName() + "] " + action.getAction().name() + " - wrong");
+            textWidget.addText("[" + player.getPlayerName() + "] " + action.getAction().name() + " - wrong");
             rollingScoreChart.addScore(faceUpCards.size() - 1);
             rollingHistogramChart.addScore(faceUpCards.size() - 1);
+            summaryChart.addText("Median=3");
             player.send(getPlayState(Signal.RESET, faceUpCards.size() - 1));
             output.display();
             // reset
@@ -131,20 +140,21 @@ public class HighLowCards implements Simulation {
 
     @Override
     public void visualise(Graphics2D graphics2D) {
-        graphics2D.setColor(Color.WHITE);
+        graphics2D.setColor(theme.getBackground());
         graphics2D.fillRect(0, 0, OutputConstants.HD_WIDTH, OutputConstants.HD_HEIGHT);
         for (int dx = 0; dx < faceUpCards.size(); dx++) {
             Card card = faceUpCards.get(dx);
             BufferedImage cardImage = getCardImage(card.getImageName());
-            graphics2D.drawImage(cardImage, dx * 50 + MARGIN, MARGIN, null);
+            graphics2D.drawImage(cardImage, dx * 60 + MARGIN, MARGIN, null);
         }
-        for (int dx = 0; dx < faceDownCards.size(); dx++) {
+        for (int dx = faceDownCards.size() - 1; dx >= 0; dx--) {
             BufferedImage cardImage = getCardImage("/images/cards/back.png");
-            graphics2D.drawImage(cardImage, (dx + faceUpCards.size() + 2) * 50 + MARGIN, MARGIN, null);
+            graphics2D.drawImage(cardImage, (dx + faceUpCards.size() + 4) * 60 + MARGIN, MARGIN, null);
         }
-        graphics2D.drawImage(rollingScoreChart.getImage(), MARGIN, OutputConstants.HD_HEIGHT - MARGIN * 2 - TEXT_HEIGHT - GRAPH_HEIGHT, null);
-        graphics2D.drawImage(rollingHistogramChart.getImage(), 2 * MARGIN + GRAPH_WIDTH, OutputConstants.HD_HEIGHT - MARGIN * 2 - TEXT_HEIGHT - GRAPH_HEIGHT, null);
-        graphics2D.drawImage(textChart.getImage(), MARGIN, OutputConstants.HD_HEIGHT - MARGIN - TEXT_HEIGHT, null);
+        graphics2D.drawImage(rollingScoreChart.getImage(), MARGIN, OutputConstants.HD_HEIGHT - 2 * MARGIN - GRAPH_HEIGHT * 2, null);
+        graphics2D.drawImage(rollingHistogramChart.getImage(), MARGIN, OutputConstants.HD_HEIGHT - MARGIN - GRAPH_HEIGHT, null);
+        graphics2D.drawImage(textWidget.getImage(), MARGIN * 2 + 720 + 200, MARGIN, null);
+        graphics2D.drawImage(summaryChart.getImage(), MARGIN * 2 + 720 + 200, OutputConstants.HD_HEIGHT - 2 * MARGIN - GRAPH_HEIGHT * 2, null);
     }
 
     private BufferedImage getCardImage(String path) {
