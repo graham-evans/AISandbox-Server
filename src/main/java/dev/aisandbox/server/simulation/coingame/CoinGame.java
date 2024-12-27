587a86093e1a060c11f28237f6b3a17f28387add
Graham Evans <gde@pobox.com>
2024-12-27 15:52:57 +0000

    update for coin images

--- src/main/java/dev/aisandbox/server/simulation/coingame/CoinGame.java
+++ src/main/java/dev/aisandbox/server/simulation/coingame/CoinGame.java
@@ -5,15 +5,18 @@ import dev.aisandbox.server.engine.Simulation;
 import dev.aisandbox.server.engine.Theme;
 import dev.aisandbox.server.engine.output.OutputConstants;
 import dev.aisandbox.server.engine.output.OutputRenderer;
-import dev.aisandbox.server.engine.widget.RollingPieChart;
-import dev.aisandbox.server.engine.widget.TextWidget;
 import dev.aisandbox.server.engine.widget.RollingLabelFrequencyStatistics;
+import dev.aisandbox.server.engine.widget.TextWidget;
 import dev.aisandbox.server.simulation.coingame.proto.CoinGameAction;
 import dev.aisandbox.server.simulation.coingame.proto.CoinGameState;
 import dev.aisandbox.server.simulation.coingame.proto.Signal;
+import dev.aisandbox.server.simulation.highlowcards.HighLowCards;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -31,6 +34,10 @@ public class CoinGame implements Simulation {
     private int currentPlayer = 0;
     private final RollingLabelFrequencyStatistics statistics = new RollingLabelFrequencyStatistics(100);
     private final TextWidget textSnapshot = statistics.createSummaryWidgetBuilder().width(200).height(200).build();
+    private BufferedImage[] rowImages;
+    private BufferedImage[] coinImages;
+    private BufferedImage logo;
+
 
     public CoinGame(final List<Player> players, final CoinScenario scenario, final Theme theme) {
         this.players = players;
@@ -44,6 +51,21 @@ public class CoinGame implements Simulation {
                 .fontName("Ariel")
                 .theme(theme)
                 .build();
+        // generate the images
+        try {
+            rowImages = CoinIcons.getRowImages(scenario.getRows().length);
+            coinImages = CoinIcons.getCoinImages(Arrays.stream(scenario.getRows()).max().getAsInt());
+        } catch (IOException e) {
+            log.error("Error loading images", e);
+        }
+        // load logo
+        try {
+            logo = ImageIO.read(HighLowCards.class.getResourceAsStream("/images/AILogo.png"));
+        } catch (Exception e) {
+            log.error("Error loading logo", e);
+            logo = new BufferedImage(OutputConstants.LOGO_WIDTH, OutputConstants.LOGO_HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        }
+        // reset the game to the initial stage
         reset();
     }
 
@@ -61,7 +83,7 @@ public class CoinGame implements Simulation {
         // try and make the move
         try {
             coins = makeMove(action.getSelectedRow(), action.getRemoveCount());
-            textWidget.addText(players.get(currentPlayer).getPlayerName() + " takes " + action.getRemoveCount() + " from row " + action.getSelectedRow());
+            textWidget.addText(players.get(currentPlayer).getPlayerName() + " takes " + action.getRemoveCount() + " from row " + action.getSelectedRow() + " leaving " + coins[action.getSelectedRow()] + ".");
             if (isGameFinished()) {
                 // current player lost
                 textWidget.addText(players.get(currentPlayer).getPlayerName() + " lost");
@@ -99,20 +121,20 @@ public class CoinGame implements Simulation {
             throw new IllegalCoinAction("Must remove between 1 and " + scenario.getMax() + " coins.");
         }
         // is the row out of the allowed indexing range
-        if (row < 1 || row > coins.length) {
+        if (row < 0 || row >= coins.length) {
             if (coins.length == 1) {
-                throw new IllegalCoinAction("Must select row 1.");
+                throw new IllegalCoinAction("Must select row 0.");
             } else {
-                throw new IllegalCoinAction("Must select row from 1 to " + coins.length + ".");
+                throw new IllegalCoinAction("Must select row from 0 to " + (coins.length - 1) + ".");
             }
         }
         // does the selected row have enough coins
-        if (coins[row-1] < amount) {
+        if (coins[row] < amount) {
             throw new IllegalCoinAction("Not enough coins in the selected row.");
         }
         // make the move
         int[] newCoins = Arrays.copyOf(coins, coins.length);
-        newCoins[row-1] -= amount;
+        newCoins[row] -= amount;
         return newCoins;
     }
 
@@ -129,22 +151,19 @@ public class CoinGame implements Simulation {
         return CoinGameState.newBuilder().setSignal(signal).build();
     }
 
-
-    @Override
-    public void close() {
-
-    }
-
     @Override
     public void visualise(Graphics2D graphics2D) {
         graphics2D.setColor(theme.getBackground());
         graphics2D.fillRect(0, 0, OutputConstants.HD_WIDTH, OutputConstants.HD_HEIGHT);
         graphics2D.setColor(theme.getText());
         for (int i = 0; i < coins.length; i++) {
-            graphics2D.drawString("Row " + (i + 1), MARGIN, MARGIN + i * 30);
-            graphics2D.drawString(Integer.toString(coins[i]), MARGIN + 50, MARGIN + i * 30);
+            graphics2D.drawImage(rowImages[i], MARGIN, MARGIN + i * CoinIcons.ROW_HEIGHT, null);
+            graphics2D.drawImage(coinImages[coins[i]], MARGIN + CoinIcons.ROW_WIDTH, MARGIN + i * CoinIcons.ROW_HEIGHT, null);
         }
         graphics2D.drawImage(textWidget.getImage(), MARGIN * 2 + 720 + 200, MARGIN, null);
         graphics2D.drawImage(textSnapshot.getImage(), MARGIN * 2 + 720 + 200, 500, null);
+        // draw logo
+        graphics2D.drawImage(logo, OutputConstants.HD_WIDTH - OutputConstants.LOGO_WIDTH - MARGIN, OutputConstants.HD_HEIGHT - OutputConstants.LOGO_HEIGHT - MARGIN, null);
+
     }
 }
