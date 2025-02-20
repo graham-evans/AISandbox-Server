590ec79f8396f841bd4dff8992077f648752b4ff
Graham Evans <gde@pobox.com>
2025-02-20 17:24:08 +0000

    add module definitions

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module AISandbox.Server.main {
+    requires ch.qos.logback.classic;
+    requires ch.qos.logback.core;
+    requires com.google.protobuf;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires static lombok;
+    requires org.apache.commons.cli;
+    requires org.apache.commons.lang3;
+    requires org.apache.commons.statistics.descriptive;
+    requires org.slf4j;
+    requires java.desktop;
+}
