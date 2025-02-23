d4f42d5e3aaebbbe83c92736fbeee53f59459f0c
Graham Evans <gde@pobox.com>
2025-02-23 17:24:35 +0000

    choose random arm

--- src/test/java/dev/aisandbox/server/simulation/bandit/MockBanditPlayer.java
+++ src/test/java/dev/aisandbox/server/simulation/bandit/MockBanditPlayer.java
@@ -3,13 +3,18 @@ package dev.aisandbox.server.simulation.bandit;
 import com.google.protobuf.GeneratedMessage;
 import dev.aisandbox.server.engine.Agent;
 import dev.aisandbox.server.simulation.bandit.proto.BanditAction;
+import dev.aisandbox.server.simulation.bandit.proto.BanditState;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Random;
+
 @Slf4j
 @RequiredArgsConstructor
 public class MockBanditPlayer implements Agent {
+
+    private final Random random = new Random();
 
     @Getter
     private final String agentName;
@@ -21,11 +26,12 @@ public class MockBanditPlayer implements Agent {
 
     @Override
     public <T extends GeneratedMessage> T receive(GeneratedMessage state, Class<T> responseType) {
+        BanditState banditState = (BanditState) state;
         if (responseType != BanditAction.class) {
             log.error("Asking for {} but I can only respond with BanditAction", responseType.getName());
             return null;
         } else {
-            return (T) BanditAction.newBuilder().setArm(0).build();
+            return (T) BanditAction.newBuilder().setArm(random.nextInt(banditState.getBanditCount())).build();
         }
     }
 
