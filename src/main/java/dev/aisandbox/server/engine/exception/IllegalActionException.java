057275ee832c4211b1fe7e272bce06814e037624
Graham Evans <gde@pobox.com>
2025-03-09 17:50:28 +0000

    document simulation breaking exceptions

--- src/main/java/dev/aisandbox/server/engine/exception/IllegalActionException.java
+++ src/main/java/dev/aisandbox/server/engine/exception/IllegalActionException.java
@@ -1,5 +1,10 @@
 package dev.aisandbox.server.engine.exception;
 
+/**
+ * Exception thrown when an agent requests something that doesn't make sense.
+ * <p>
+ * Will result in the simulation being aborted.
+ */
 public class IllegalActionException extends SimulationException {
     public IllegalActionException(String message) {
         super(message);
--- src/main/java/dev/aisandbox/server/engine/exception/SimulationException.java
+++ src/main/java/dev/aisandbox/server/engine/exception/SimulationException.java
@@ -1,5 +1,8 @@
 package dev.aisandbox.server.engine.exception;
 
+/**
+ * Generic simulation exception, used for all exceptions that occur during the execution of a simulation.
+ */
 public class SimulationException extends Exception {
 
     public SimulationException(String message) {
