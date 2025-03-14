ee30c4ed0d61c4a2c97d4292d6a8227a8a62bde8
Graham Evans <gde@pobox.com>
2025-03-14 21:31:03 +0000

    show documentation output and test for no exceptions

---
+++ src/test/java/dev/aisandbox/server/simulation/DocumentationTests.java
@@ -0,0 +1,29 @@
+package dev.aisandbox.server.simulation;
+
+import dev.aisandbox.launcher.SandboxServerCLIApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+@Slf4j
+public class DocumentationTests {
+
+    @ParameterizedTest
+    @EnumSource(SimulationEnumeration.class)
+    public void testDocumentationGeneration(SimulationEnumeration simulationEnumeration) {
+        log.info("Testing generation of docs for {}",simulationEnumeration.name());
+
+        SandboxServerCLIApplication app = new SandboxServerCLIApplication();
+
+        app.run("--help","-s", simulationEnumeration.getBuilder().getSimulationName());
+    }
+
+    @Test
+    public void testList() {
+        SandboxServerCLIApplication app = new SandboxServerCLIApplication();
+        app.run("--list");
+    }
+
+
+}
