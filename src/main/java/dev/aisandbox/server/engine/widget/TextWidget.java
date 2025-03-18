b57ed6db982836509edbb842465ed279605a9d8a
Graham Evans <gde@pobox.com>
2025-03-18 21:01:36 +0000

    wrap long lines

--- src/main/java/dev/aisandbox/server/engine/widget/TextWidget.java
+++ src/main/java/dev/aisandbox/server/engine/widget/TextWidget.java
@@ -8,71 +8,91 @@ import lombok.experimental.Accessors;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 import java.util.StringJoiner;
 
 public class TextWidget {
     @Getter(AccessLevel.PACKAGE)
-    private final List<String> lines = new ArrayList<>();
     private final int width;
     private final int height;
-    private final double lineHeight;
+    private final int lineHeight;
     @Getter(AccessLevel.PACKAGE)
     private final Theme theme;
-    private final Font font;
-    private final int maxLines;
+    private final int PADDING = 10;
     // internal state
-    private BufferedImage image = null;
+    @Getter
+    private final BufferedImage image;
+    private final Graphics2D graphics;
+    private  BufferedImage blankLine;
+    private final FontMetrics fontMetrics;
+    private final int descender;
 
     private TextWidget(int width, int height, int fontHeight, String fontName, Theme theme) {
         this.width = width;
         this.height = height;
-        this.lineHeight = fontHeight * 1.1;
+        this.lineHeight = fontHeight + 2;
         this.theme = theme;
-        // create font
-        font = new Font(fontName, Font.PLAIN, fontHeight);
-        maxLines = (int)(height / lineHeight);
+        // create blank image
+        image = GraphicsUtils.createBlankImage(width,height,theme.getWidgetBackground());
+        graphics = image.createGraphics();
+        blankLine = GraphicsUtils.createBlankImage(width-PADDING,lineHeight,theme.getWidgetBackground());
+        // setup text
+        graphics.setColor(theme.getText());
+        Font font = new Font(fontName, Font.PLAIN, fontHeight);
+        graphics.setFont(font);
+        GraphicsUtils.setupRenderingHints(graphics);
+        fontMetrics = graphics.getFontMetrics();
+        descender = fontMetrics.getDescent();
     }
 
     public static TextWidgetBuilder builder() {
         return new TextWidgetBuilder();
     }
 
+    private void addTextLine(final String text) {
+        // move image one line up
+        BufferedImage scrollImage = image.getSubimage(PADDING,PADDING+lineHeight,width-PADDING,height-PADDING*2-lineHeight);
+        graphics.drawImage(scrollImage,PADDING,PADDING,null);
+        // blank the bottom line
+        graphics.drawImage(blankLine,PADDING,height-PADDING-lineHeight,null);
+        // draw the text line
+        graphics.drawString(text,PADDING,height-PADDING- descender);
+    }
+
     public void addText(final String text) {
-        lines.add(text);
-        while (lines.size() > maxLines) {
-            lines.removeFirst();
+        // is this a simple line of text
+        if (fontMetrics.stringWidth(text) <= width - PADDING*2) {
+            addTextLine(text);
+        } else {
+            // try to split the line-up
+            StringBuilder line = new StringBuilder();
+            for (String word : text.split(" ")) {
+                if (line.isEmpty()) { // case 1 - first word always gets added
+                    line.append(word);
+                    line.append(" ");
+                }
+                if (fontMetrics.stringWidth(line.toString()+word) <= width - PADDING*2) { // case 2 - room for another word
+                    line.append(word);
+                    line.append(" ");
+                } else {
+                    // write the line
+                    addTextLine(line.toString());
+                    // start the next line
+                    line.setLength(0);
+                    line.append(word);
+                }
+            }
+            if (!line.isEmpty()) {
+                addTextLine(line.toString());
+            }
         }
-        // invalidate image
-        image = null;
+
     }
 
     public void reset() {
-        lines.clear();
-        image = null;
-    }
-
-    public BufferedImage getImage() {
-        if (image == null) {
-            image = renderText();
-        }
-        return image;
-    }
-
-    private BufferedImage renderText() {
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g = image.createGraphics();
-        GraphicsUtils.setupRenderingHints(g);
-        g.setColor(theme.getBackground());
-        g.setColor(Color.LIGHT_GRAY);
-        g.fillRect(0, 0, width, height);
-        g.setColor(theme.getText());
-        g.setFont(font);
-        for (int i = 0; i < lines.size(); i++) {
-            g.drawString(lines.get(i), 0, (int)((i + 1) * lineHeight));
-        }
-        return image;
+        graphics.setColor(theme.getWidgetBackground());
+        graphics.fillRect(0, 0, width, height);
+        graphics.setColor(theme.getText());
     }
 
     @Setter
