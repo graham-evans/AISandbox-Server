8dc1527d2267bc4000d6c1911a72625303eef980
Graham Evans <gde@pobox.com>
2025-03-24 17:38:16 +0000

    add font loading

--- src/main/java/dev/aisandbox/server/engine/output/OutputConstants.java
+++ src/main/java/dev/aisandbox/server/engine/output/OutputConstants.java
@@ -5,8 +5,10 @@ import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.List;
 
 @UtilityClass
 @Slf4j
@@ -24,11 +26,15 @@ public class OutputConstants {
     public static final int LEFT_MARGIN = 50;
     public static final int RIGHT_MARGIN = 50;
     public static final int WIDGET_SPACING = 50;
-    public static final int LOG_FONT_HEIGHT = 14;
-    public static final String LOG_FONT = "Arial";
+    public static final int LOG_FONT_HEIGHT = 16;
+    public static final Font TITLE_FONT;
+    public static final Font LOG_FONT;
+    public static final Font STATISTICS_FONT;
 
 
     public static final BufferedImage LOGO;
+
+    private static final List<String> FONT_LIST = List.of("/fonts/Arimo-VariableFont_wght.ttf", "/fonts/Hack-Regular.ttf");
 
     static {
         // load logo
@@ -40,5 +46,21 @@ public class OutputConstants {
             i = new BufferedImage(LOGO_WIDTH, LOGO_HEIGHT, BufferedImage.TYPE_INT_RGB);
         }
         LOGO = i;
+        // Load fonts
+        GraphicsEnvironment GE = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        try {
+            for (String path : FONT_LIST) {
+                log.debug("Loading font from {}", path);
+                Font font = Font.createFont(Font.TRUETYPE_FONT, OutputConstants.class.getResourceAsStream(path));
+                log.debug("Registering font {}", font.getFontName());
+                GE.registerFont(font);
+            }
+        } catch (FontFormatException | IOException e) {
+            log.error("Error loading fonts", e);
+        }
+        // create base fonts
+        TITLE_FONT = new Font("Arimo Regular", Font.BOLD, TITLE_HEIGHT);
+        LOG_FONT = new Font("Hack Regular", Font.PLAIN, LOG_FONT_HEIGHT);
+        STATISTICS_FONT=new Font("Arimo Regular", Font.PLAIN, 40);
     }
 }
