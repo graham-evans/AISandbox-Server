a78d224fc5004ad647ca70bcf227de200bafa24f
Graham Evans <gde@pobox.com>
2025-03-24 22:18:01 +0000

    test with dark theme

--- src/test/java/dev/aisandbox/server/simulation/highlowcards/TestRunHighLowCards.java
+++ src/test/java/dev/aisandbox/server/simulation/highlowcards/TestRunHighLowCards.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 public class TestRunHighLowCards {
 
     @Test
-    public void testRunHighLowCards() {
+    public void testRunHighLowCardsLight() {
         assertDoesNotThrow(() -> {
             // create simulation
             SimulationBuilder simulationBuilder = new HighLowCardsBuilder();
@@ -26,7 +26,34 @@ public class TestRunHighLowCards {
             // create simulation
             Simulation sim = simulationBuilder.build(agents, Theme.LIGHT, new Random());
             // create output directory
-            File outputDirectory = new File("build/test/highLowCards");
+            File outputDirectory = new File("build/test/highLowCards/Light");
+            outputDirectory.mkdirs();
+            // create output
+            OutputRenderer out = new BitmapOutputRenderer();
+            out.setSkipFrames(100);
+            out.setOutputDirectory(outputDirectory);
+            out.setup(sim);
+            // start simulation
+            for (int step = 0; step < 1000; step++) {
+                sim.step(out);
+            }
+            // finish simulation
+            sim.close();
+            agents.forEach(Agent::close);
+        });
+    }
+
+    @Test
+    public void testRunHighLowCardsDark() {
+        assertDoesNotThrow(() -> {
+            // create simulation
+            SimulationBuilder simulationBuilder = new HighLowCardsBuilder();
+            // create players
+            List<Agent> agents = List.of(new MockPlayer());
+            // create simulation
+            Simulation sim = simulationBuilder.build(agents, Theme.DARK, new Random());
+            // create output directory
+            File outputDirectory = new File("build/test/highLowCards/Dark");
             outputDirectory.mkdirs();
             // create output
             OutputRenderer out = new BitmapOutputRenderer();
