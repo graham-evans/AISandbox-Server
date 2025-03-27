2519d3fe2fe188220da63f9f342f4cb3c0dd7306
Graham Evans <gde@pobox.com>
2025-03-27 09:26:07 +0000

    update colours and add warm theme

--- src/main/java/dev/aisandbox/server/engine/Theme.java
+++ src/main/java/dev/aisandbox/server/engine/Theme.java
@@ -12,15 +12,16 @@ public enum Theme {
 
     LIGHT(
             Color.decode("#efefef"), // background
-            Color.BLACK, // text
-            Color.WHITE, // widget background
-            Color.WHITE, // graph background
-            Color.DARK_GRAY, // graph outline (axis)
-            Color.WHITE, // baize (play area)
+            Color.decode("#4c4c4d"), // text
+            Color.decode("#fffaf0"), // widget background
+            Color.decode("#fffaf0"), // graph background
+            Color.decode("#4c4c4d"), // graph outline (axis)
+            Color.decode("#fffaf0"), // baize (play area)
+            Color.decode("#07144b"), // graph colour
 
-            Color.decode("#014f89"),
-            Color.decode("#2a6f97"),
-            Color.decode("#01497c"),
+            Color.decode("#85d8ef"),
+            Color.decode("#85d8ef"),
+            Color.decode("#85d8ef"),
 
             Color.decode("#640d14"),
             Color.decode("#800e13"),
@@ -29,14 +30,38 @@ public enum Theme {
             Color.decode("#ffa200"),
             Color.decode("#ffaa00"),
             Color.decode("#ff9500")
-    ), DARK(
-            Color.BLACK,
-            Color.WHITE,
-            Color.lightGray,
-            Color.WHITE,
-            Color.DARK_GRAY,
+    ),
+    WARM(
+            Color.decode("#fef4e1"), // background
+            Color.decode("#494949"), // text
+            Color.decode("#ffffff"), // widget background
+            Color.decode("#ffffff"), // graph background
+            Color.decode("#494949"), // graph outline (axis)
+            Color.decode("#ffffff"), // baize (play area)
+            Color.decode("#ea423d"), // graph colour 1
 
-            Color.decode("#006400"),
+            Color.decode("#038da2"),
+            Color.decode("#038da2"),
+            Color.decode("#038da2"),
+
+            Color.decode("#640d14"),
+            Color.decode("#800e13"),
+            Color.decode("#38040e"),
+
+            Color.decode("#ffa200"),
+            Color.decode("#ffaa00"),
+            Color.decode("#ff9500")
+    ),
+
+
+    DARK(
+            Color.decode("#000d43"), // background
+            Color.decode("#ffffff"), // text
+            Color.decode("#001155"), // widget background
+            Color.decode("#ffe0a6"), // graph background
+            Color.decode("#000d43"), // graph outline (axis)
+            Color.decode("#001155"), // baize (play area)
+            Color.decode("#ff0800"), // graph colour 1
 
             Color.decode("#014f89"),
             Color.decode("#2a6f97"),
@@ -57,6 +82,8 @@ public enum Theme {
     private final Color graphBackground;
     private final Color graphOutlineColor;
     private final Color baize;
+
+    private final Color graphColor1;
 
     private final Color agent1Main;
     private final Color agent1Highlight;
