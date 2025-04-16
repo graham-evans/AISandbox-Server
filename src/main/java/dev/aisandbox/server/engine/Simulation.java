db1a084cf63fa606198865aa1a7128516783bc1b
graham-evans <graham-evans@users.noreply.github.com>
2025-04-16 16:50:48 +0100

    add javadoc

--- src/main/java/dev/aisandbox/server/engine/Simulation.java
+++ src/main/java/dev/aisandbox/server/engine/Simulation.java
@@ -4,13 +4,42 @@ import dev.aisandbox.server.engine.exception.SimulationException;
 import dev.aisandbox.server.engine.output.OutputRenderer;
 import java.awt.Graphics2D;
 
+/**
+ * Interface which all simulations must implement.
+ */
 public interface Simulation {
 
+  /**
+   * Finish the simulation, closing any resources as required.
+   *
+   * <p>Has a no-opp default.
+   */
   default void close() {
     // no action
   }
 
+  /**
+   * Perform a step in the simulation.
+   *
+   * <p>Optionally trigger an update to the output (possibly more than once).</p>
+   *
+   * @param output the {@link dev.aisandbox.server.engine.output.OutputRenderer} to trigger
+   *               updates.
+   * @throws SimulationException an exception which denotes the simulation has failed and should be
+   *                             shutdown.
+   */
   void step(OutputRenderer output) throws SimulationException;
 
+  /**
+   * Visualise the current state of the simulation, called from a
+   * {@link dev.aisandbox.server.engine.output.OutputRenderer}.
+   *
+   * <p>The {@link java.awt.Graphics2D} is for a display space with the same dimensions as an HD
+   * Screen. Defined in {@link dev.aisandbox.server.engine.output.OutputConstants} as
+   * {@link dev.aisandbox.server.engine.output.OutputConstants#HD_WIDTH} by
+   * {@link dev.aisandbox.server.engine.output.OutputConstants#HD_HEIGHT}
+   *
+   * @param graphics2D the graphics object used to render the state.
+   */
   void visualise(Graphics2D graphics2D);
 }
