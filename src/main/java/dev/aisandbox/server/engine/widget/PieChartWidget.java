e45db90b5d67469edaef52564e6912aa022425ad
graham-evans <graham-evans@users.noreply.github.com>
2025-05-01 22:29:58 +0100

    change font and colour

--- src/main/java/dev/aisandbox/server/engine/widget/PieChartWidget.java
+++ src/main/java/dev/aisandbox/server/engine/widget/PieChartWidget.java
@@ -1,6 +1,7 @@
 package dev.aisandbox.server.engine.widget;
 
 import dev.aisandbox.server.engine.Theme;
+import dev.aisandbox.server.engine.output.OutputConstants;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
@@ -95,8 +96,8 @@ public class PieChartWidget {
       int textY = (int) (startY + pieDiameter / 2 + Math.sin(midAngle) * pieDiameter / 3);
 
       //     g.fillOval(textX-3,textY-3,6,6);
-      GraphicsUtils.drawCenteredText(g, textX - 20, textY - 10, 40, 20, slice.title,
-          new Font("Arial", Font.PLAIN, 12), theme.getText());
+      GraphicsUtils.drawCenteredText(g, textX - 20, textY - 10, 40, 20, slice.title, OutputConstants.LOG_FONT
+          , Color.WHITE);
       startAngle += angle;
     }
     return image;
