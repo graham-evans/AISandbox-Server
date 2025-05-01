138561447a455682712de672371d099f06b387a6
graham-evans <graham-evans@users.noreply.github.com>
2025-05-01 23:11:29 +0100

    add title, baize and log widget

--- src/main/java/dev/aisandbox/server/simulation/maze/MazeRunner.java
+++ src/main/java/dev/aisandbox/server/simulation/maze/MazeRunner.java
@@ -1,17 +1,25 @@
 package dev.aisandbox.server.simulation.maze;
 
+import static dev.aisandbox.server.engine.output.OutputConstants.BOTTOM_MARGIN;
 import static dev.aisandbox.server.engine.output.OutputConstants.HD_HEIGHT;
 import static dev.aisandbox.server.engine.output.OutputConstants.HD_WIDTH;
+import static dev.aisandbox.server.engine.output.OutputConstants.LEFT_MARGIN;
 import static dev.aisandbox.server.engine.output.OutputConstants.LOGO;
 import static dev.aisandbox.server.engine.output.OutputConstants.LOGO_HEIGHT;
 import static dev.aisandbox.server.engine.output.OutputConstants.LOGO_WIDTH;
-import static dev.aisandbox.server.engine.output.OutputConstants.MARGIN;
+import static dev.aisandbox.server.engine.output.OutputConstants.LOG_FONT;
+import static dev.aisandbox.server.engine.output.OutputConstants.RIGHT_MARGIN;
+import static dev.aisandbox.server.engine.output.OutputConstants.TITLE_HEIGHT;
+import static dev.aisandbox.server.engine.output.OutputConstants.TOP_MARGIN;
+import static dev.aisandbox.server.engine.output.OutputConstants.WIDGET_SPACING;
 
 import dev.aisandbox.server.engine.Agent;
 import dev.aisandbox.server.engine.Simulation;
 import dev.aisandbox.server.engine.Theme;
 import dev.aisandbox.server.engine.output.OutputRenderer;
 import dev.aisandbox.server.engine.output.SpriteLoader;
+import dev.aisandbox.server.engine.widget.TextWidget;
+import dev.aisandbox.server.engine.widget.TitleWidget;
 import dev.aisandbox.server.simulation.maze.proto.MazeAction;
 import dev.aisandbox.server.simulation.maze.proto.MazeResult;
 import dev.aisandbox.server.simulation.maze.proto.MazeState;
@@ -26,10 +34,26 @@ import lombok.extern.slf4j.Slf4j;
 public final class MazeRunner implements Simulation {
 
   public static final int SPRITE_SIZE = 25;
+  // layout constants
+  private static final int BAIZE_HEIGHT =
+      HD_HEIGHT - TOP_MARGIN - BOTTOM_MARGIN - TITLE_HEIGHT - WIDGET_SPACING; // 1173
+  private static final int BAIZE_WIDTH = BAIZE_HEIGHT * 4 / 3; // 880
+  private static final int LOG_WIDTH =
+      HD_WIDTH - LEFT_MARGIN - RIGHT_MARGIN - BAIZE_WIDTH - WIDGET_SPACING;
+  private static final int MAZE_START_X = LEFT_MARGIN + (BAIZE_WIDTH - 1000) / 2;
+  private static final int LOG_HEIGHT = (BAIZE_HEIGHT - WIDGET_SPACING) / 2;
+  private static final int MAZE_START_Y =
+      TOP_MARGIN + TITLE_HEIGHT + WIDGET_SPACING + (BAIZE_HEIGHT - 750) / 2;
+  // Rewards
   private static final double REWARD_STEP = -1.0;
   private static final double REWARD_HIT_WALL = -10.0;
   private static final double REWARD_GOAL = +1000.0;
+
   private static final int EPISODE_LENGTH = 2000;
+  // widgets
+  private final TitleWidget titleWidget;
+  private final TextWidget logWidget;
+  // maze settings
   private final MazeSize mazeSize;
   private final MazeType mazeType;
   private final Theme theme;
@@ -42,7 +66,9 @@ public final class MazeRunner implements Simulation {
   private BufferedImage mazeImage = null;
   private Cell currentCell;
   private String episodeID;
+
   private double episodeScore;
+
 
   public MazeRunner(Agent agent, MazeSize mazeSize, MazeType mazeType, Theme theme, Random random) {
     this.agent = agent;
@@ -53,6 +79,9 @@ public final class MazeRunner implements Simulation {
     // load images
     sprites = SpriteLoader.loadSpritesFromResources("/images/maze/bridge.png", SPRITE_SIZE,
         SPRITE_SIZE);
+    titleWidget = TitleWidget.builder().title("Maze - " + mazeType.name()).theme(theme).build();
+    logWidget = TextWidget.builder().width(LOG_WIDTH).height(LOG_HEIGHT).font(LOG_FONT).theme(theme)
+        .build();
     // create a new maze
     initialiseMaze();
   }
@@ -85,8 +114,10 @@ public final class MazeRunner implements Simulation {
     } else {
       // hit wall - dont move
       score = REWARD_HIT_WALL;
+
     }
     // report result
+    logWidget.addText("Move "+direction.name()+" ("+score+")");
     episodeScore += score;
     agent.send(MazeResult.newBuilder().setStartX(startX).setStartY(startY)
         .setEndX(currentCell.getPositionX()).setEndY(currentCell.getPositionY())
@@ -100,6 +131,7 @@ public final class MazeRunner implements Simulation {
     }
     // SPECIAL CASE  - end of the episode?
     if (stepsLeft == 0) {
+      logWidget.addText("Episode finished, resetting maze");
       initialiseMaze();
     }
 
@@ -109,27 +141,41 @@ public final class MazeRunner implements Simulation {
   public void visualise(Graphics2D graphics2D) {
     graphics2D.setColor(theme.getBackground());
     graphics2D.fillRect(0, 0, HD_WIDTH, HD_HEIGHT);
-
+    // draw title
+    graphics2D.drawImage(titleWidget.getImage(), 0, TOP_MARGIN, null);
+    graphics2D.drawImage(LOGO, HD_WIDTH - LOGO_WIDTH - RIGHT_MARGIN,
+        (TOP_MARGIN + TITLE_HEIGHT + WIDGET_SPACING - LOGO_HEIGHT) / 2, null);
+    // draw baize
+    graphics2D.setColor(theme.getBaize());
+    graphics2D.fillRect(LEFT_MARGIN, TOP_MARGIN + TITLE_HEIGHT + WIDGET_SPACING, BAIZE_WIDTH,
+        BAIZE_HEIGHT);
     // draw maze
-    graphics2D.drawImage(mazeImage, MARGIN, MARGIN,
+    graphics2D.drawImage(mazeImage, MAZE_START_X, MAZE_START_Y,
         maze.getWidth() * SPRITE_SIZE * maze.getZoomLevel(),
         maze.getHeight() * SPRITE_SIZE * maze.getZoomLevel(), null);
     // draw the player
     graphics2D.setColor(theme.getAgent1Main());
-    graphics2D.fillOval(currentCell.getPositionX() * mazeSize.getZoomLevel() * SPRITE_SIZE + MARGIN,
-        MARGIN + currentCell.getPositionY() * mazeSize.getZoomLevel() * SPRITE_SIZE,
+    graphics2D.fillOval(
+        currentCell.getPositionX() * mazeSize.getZoomLevel() * SPRITE_SIZE + MAZE_START_X,
+        MAZE_START_Y + currentCell.getPositionY() * mazeSize.getZoomLevel() * SPRITE_SIZE,
         mazeSize.getZoomLevel() * SPRITE_SIZE, mazeSize.getZoomLevel() * SPRITE_SIZE);
-    // draw logo
-    graphics2D.drawImage(LOGO, HD_WIDTH - LOGO_WIDTH - MARGIN, HD_HEIGHT - LOGO_HEIGHT - MARGIN,
-        null);
+    // draw log
+    graphics2D.drawImage(logWidget.getImage(), HD_WIDTH - RIGHT_MARGIN - LOG_WIDTH,
+        TOP_MARGIN + TITLE_HEIGHT + WIDGET_SPACING + LOG_HEIGHT + WIDGET_SPACING, null);
   }
 
   private void initialiseMaze() {
+    // create a new maze
     maze = MazeGenerator.generateMaze(mazeSize, mazeType, random);
+    // reset the steps
     stepsLeft = EPISODE_LENGTH;
+    // reset the score
     episodeScore = 0.0;
+    // generate maze image
     mazeImage = renderMaze(maze);
+    // set start cell
     currentCell = maze.getStartCell();
+    // set episode ID
     episodeID = UUID.randomUUID().toString();
   }
 
@@ -161,11 +207,11 @@ public final class MazeRunner implements Simulation {
       g.drawImage(sprites.get(icon), c.getPositionX() * SPRITE_SIZE, c.getPositionY() * SPRITE_SIZE,
           null);
     }
-    Cell start = maze.getStartCell();
+ /*   Cell start = maze.getStartCell();
     if (start != null) {
       g.drawImage(sprites.get(17), start.getPositionX() * SPRITE_SIZE,
           start.getPositionY() * SPRITE_SIZE, null);
-    }
+    } */
     Cell finish = maze.getEndCell();
     if (finish != null) {
       g.drawImage(sprites.get(18), finish.getPositionX() * SPRITE_SIZE,
