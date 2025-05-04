0aa26d5b71635448592d89af756db88cdd27967a
graham-evans <graham-evans@users.noreply.github.com>
2025-05-04 19:12:20 +0100

    add log and pie chart to UI

--- src/main/java/dev/aisandbox/server/simulation/mine/MineHunterRuntime.java
+++ src/main/java/dev/aisandbox/server/simulation/mine/MineHunterRuntime.java
@@ -7,7 +7,9 @@ import static dev.aisandbox.server.engine.output.OutputConstants.LEFT_MARGIN;
 import static dev.aisandbox.server.engine.output.OutputConstants.LOGO;
 import static dev.aisandbox.server.engine.output.OutputConstants.LOGO_HEIGHT;
 import static dev.aisandbox.server.engine.output.OutputConstants.LOGO_WIDTH;
+import static dev.aisandbox.server.engine.output.OutputConstants.LOG_FONT;
 import static dev.aisandbox.server.engine.output.OutputConstants.RIGHT_MARGIN;
+import static dev.aisandbox.server.engine.output.OutputConstants.STATISTICS_HEIGHT;
 import static dev.aisandbox.server.engine.output.OutputConstants.TITLE_HEIGHT;
 import static dev.aisandbox.server.engine.output.OutputConstants.TOP_MARGIN;
 import static dev.aisandbox.server.engine.output.OutputConstants.WIDGET_SPACING;
@@ -18,13 +20,15 @@ import dev.aisandbox.server.engine.Theme;
 import dev.aisandbox.server.engine.output.OutputConstants;
 import dev.aisandbox.server.engine.output.OutputRenderer;
 import dev.aisandbox.server.engine.output.SpriteLoader;
+import dev.aisandbox.server.engine.widget.GraphicsUtils;
+import dev.aisandbox.server.engine.widget.RollingPieChartWidget;
+import dev.aisandbox.server.engine.widget.TextWidget;
 import dev.aisandbox.server.engine.widget.TitleWidget;
 import dev.aisandbox.server.simulation.mine.proto.FlagAction;
 import dev.aisandbox.server.simulation.mine.proto.MazeSignal;
 import dev.aisandbox.server.simulation.mine.proto.MineAction;
 import dev.aisandbox.server.simulation.mine.proto.MineResult;
 import dev.aisandbox.server.simulation.mine.proto.MineState;
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.util.List;
@@ -38,10 +42,14 @@ public final class MineHunterRuntime implements Simulation {
   private static final int BAIZE_HEIGHT =
       HD_HEIGHT - TOP_MARGIN - BOTTOM_MARGIN - TITLE_HEIGHT - WIDGET_SPACING; // 1173
   private static final int BAIZE_WIDTH = BAIZE_HEIGHT * 4 / 3; // 880
+  private static final int BAIZE_SPACING = (BAIZE_HEIGHT - 800-STATISTICS_HEIGHT) / 3;
 
   private static final int BOARD_START_X = LEFT_MARGIN + (BAIZE_WIDTH - 800) / 2;
   private static final int BOARD_START_Y =
-      TOP_MARGIN + TITLE_HEIGHT + WIDGET_SPACING + (BAIZE_HEIGHT - 800) / 2;
+      TOP_MARGIN + TITLE_HEIGHT + WIDGET_SPACING + BAIZE_SPACING *2+STATISTICS_HEIGHT;
+  private static final int LOG_HEIGHT = (BAIZE_HEIGHT - WIDGET_SPACING) / 2;
+  private static final int LOG_WIDTH =
+      HD_WIDTH - LEFT_MARGIN - RIGHT_MARGIN - BAIZE_WIDTH - WIDGET_SPACING;
   // agents
   private final Agent agent;
   // puzzle elements
@@ -50,9 +58,9 @@ public final class MineHunterRuntime implements Simulation {
   private final Theme theme;
   private final String sessionID = UUID.randomUUID().toString();
   private final List<BufferedImage> sprites;
-  //    private SuccessRateGraph winRateGraph = new SuccessRateGraph();
-//    private BufferedImage winRateGraphImage = null;
+  private final RollingPieChartWidget pieChartWidget;
   private final TitleWidget titleWidget;
+  private final TextWidget logWidget;
   private final long boardsWon = 0;
   private final long boardsLost = 0;
   private Board board = null;
@@ -68,6 +76,10 @@ public final class MineHunterRuntime implements Simulation {
     // load sprites
     sprites = SpriteLoader.loadSpritesFromResources("/images/mine/grid.png", 40, 40);
     titleWidget = TitleWidget.builder().title("Mine Hunter").theme(theme).build();
+    logWidget = TextWidget.builder().width(LOG_WIDTH).height(LOG_HEIGHT).font(LOG_FONT).theme(theme)
+        .build();
+    pieChartWidget = RollingPieChartWidget.builder().width(LOG_WIDTH).height(LOG_HEIGHT)
+        .title("% success of the last 200 episodes.").theme(theme).build();
     // create first board
     getNewBoard();
   }
@@ -90,8 +102,10 @@ public final class MineHunterRuntime implements Simulation {
     MineAction action = agent.receive(getState(), MineAction.class);
     // place flag or dig
     if (action.getAction().equals(FlagAction.PLACE_FLAG)) {
+      logWidget.addText(agent.getAgentName()+": placing flag @ "+action.getX()+","+action.getY());
       board.placeFlag(action.getX(), action.getY());
     } else {
+      logWidget.addText(agent.getAgentName()+": uncovering flag @ "+action.getX()+","+action.getY());
       board.uncover(action.getX(), action.getY());
     }
     // report state
@@ -108,8 +122,12 @@ public final class MineHunterRuntime implements Simulation {
     agent.send(rBuilder.build());
     if (board.getState() == GameState.WON) {
       // update stats
+      logWidget.addText(agent.getAgentName()+": won");
+      pieChartWidget.addValue("win",theme.getAgent1Main());
     } else if (board.getState() == GameState.LOST) {
       // update stats
+      logWidget.addText(agent.getAgentName()+": lost");
+      pieChartWidget.addValue("loss",theme.getAgent2Main());
     }
     // draw the screen
     output.display();
@@ -133,23 +151,29 @@ public final class MineHunterRuntime implements Simulation {
 
   @Override
   public void visualise(Graphics2D graphics2D) {
+    GraphicsUtils.setupRenderingHints(graphics2D);
     graphics2D.setColor(theme.getBackground());
     graphics2D.fillRect(0, 0, HD_WIDTH, HD_HEIGHT);
     // draw title
     graphics2D.drawImage(titleWidget.getImage(), 0, TOP_MARGIN, null);
     graphics2D.drawImage(LOGO, HD_WIDTH - LOGO_WIDTH - RIGHT_MARGIN,
         (TOP_MARGIN + TITLE_HEIGHT + WIDGET_SPACING - LOGO_HEIGHT) / 2, null);
+    // draw log
+    graphics2D.drawImage(logWidget.getImage(), HD_WIDTH - RIGHT_MARGIN - LOG_WIDTH,
+        TOP_MARGIN + TITLE_HEIGHT + WIDGET_SPACING + LOG_HEIGHT + WIDGET_SPACING, null);
+    // draw pie chart
+    graphics2D.drawImage(pieChartWidget.getImage(), HD_WIDTH - RIGHT_MARGIN - LOG_WIDTH,
+        TOP_MARGIN + TITLE_HEIGHT + WIDGET_SPACING, null);
     // draw baize
     graphics2D.setColor(theme.getBaize());
     graphics2D.fillRect(LEFT_MARGIN, TOP_MARGIN + TITLE_HEIGHT + WIDGET_SPACING, BAIZE_WIDTH,
         BAIZE_HEIGHT);
-    // draw mines
-    graphics2D.setColor(Color.BLACK);
-    graphics2D.setFont(OutputConstants.LOG_FONT);
-    graphics2D.drawString("Mines Remaining : " + board.getUnfoundMines(), 1200, 500);
+    // draw score
+    GraphicsUtils.drawCenteredText(graphics2D,LEFT_MARGIN,TOP_MARGIN+TITLE_HEIGHT+WIDGET_SPACING+BAIZE_SPACING,BAIZE_WIDTH,OutputConstants.STATISTICS_HEIGHT,"Mines Remaining : " + board.getUnfoundMines(),OutputConstants.STATISTICS_FONT,theme.getText());
     // transform for drawing the board
     graphics2D.translate(BOARD_START_X, BOARD_START_Y);
     graphics2D.scale(scale, scale);
+    // draw mines
     for (int x = 0; x < board.getWidth(); x++) {
       for (int y = 0; y < board.getHeight(); y++) {
         Cell c = board.getCell(x, y);
