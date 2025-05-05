69331333a8e5bb4f53d93a53f6fb9477929ff743
graham-evans <graham-evans@users.noreply.github.com>
2025-05-05 23:29:19 +0100

    start refactoring

--- src/main/java/dev/aisandbox/server/simulation/twisty/TwistySimulation.java
+++ src/main/java/dev/aisandbox/server/simulation/twisty/TwistySimulation.java
@@ -1,13 +1,13 @@
 package dev.aisandbox.server.simulation.twisty;
 
 
+import static dev.aisandbox.server.engine.output.OutputConstants.BOTTOM_MARGIN;
 import static dev.aisandbox.server.engine.output.OutputConstants.HD_HEIGHT;
 import static dev.aisandbox.server.engine.output.OutputConstants.HD_WIDTH;
 import static dev.aisandbox.server.engine.output.OutputConstants.LEFT_MARGIN;
 import static dev.aisandbox.server.engine.output.OutputConstants.LOGO;
 import static dev.aisandbox.server.engine.output.OutputConstants.LOGO_HEIGHT;
 import static dev.aisandbox.server.engine.output.OutputConstants.LOGO_WIDTH;
-import static dev.aisandbox.server.engine.output.OutputConstants.MARGIN;
 import static dev.aisandbox.server.engine.output.OutputConstants.RIGHT_MARGIN;
 import static dev.aisandbox.server.engine.output.OutputConstants.TITLE_HEIGHT;
 import static dev.aisandbox.server.engine.output.OutputConstants.TOP_MARGIN;
@@ -17,7 +17,9 @@ import dev.aisandbox.server.engine.Agent;
 import dev.aisandbox.server.engine.Simulation;
 import dev.aisandbox.server.engine.Theme;
 import dev.aisandbox.server.engine.output.OutputRenderer;
+import dev.aisandbox.server.engine.widget.RollingIconWidget;
 import dev.aisandbox.server.engine.widget.TitleWidget;
+import dev.aisandbox.server.simulation.twisty.model.CompiledMove;
 import dev.aisandbox.server.simulation.twisty.model.Move;
 import dev.aisandbox.server.simulation.twisty.model.TwistyPuzzle;
 import dev.aisandbox.server.simulation.twisty.proto.TwistyAction;
@@ -35,10 +37,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public final class TwistySimulation implements Simulation {
 
+  // simulation constants
   private static final int SCRAMBLE_MOVES = 200;
-  private static final int HISTORY_WIDTH = 9;
-  private static final int HISTORY_HEIGHT = 5;
-  private static final int MOVE_HISTORY_MAX = HISTORY_WIDTH * HISTORY_HEIGHT;
+  // ui constants
+  private static final int WIDGET_WIDTH =
+      HD_WIDTH - LEFT_MARGIN - TwistyPuzzle.WIDTH - RIGHT_MARGIN - WIDGET_SPACING;
+  private static final int WIDGET_HEIGHT =
+      (HD_HEIGHT - TOP_MARGIN - TITLE_HEIGHT - WIDGET_SPACING * 2 - BOTTOM_MARGIN) / 2;
+
   // agents
   private final Agent agent;
   private final TwistyPuzzle puzzle;
@@ -50,7 +56,7 @@ public final class TwistySimulation implements Simulation {
   private final int MAX_MOVES = 1000;
   // UI elements
   private final TitleWidget titleWidget;
-  private final List<String> moveHistory = new ArrayList<>();
+  private final RollingIconWidget moveHistoryWidget;
   String savedState;
   List<String> actions = new ArrayList<>();
   int moves;
@@ -64,15 +70,19 @@ public final class TwistySimulation implements Simulation {
     this.startSolved = startSolved;
     this.theme = theme;
     this.random = random;
-    // setup puzzle
-    initialisePuzzle();
+
     // setup ui
-    titleWidget = TitleWidget.builder().title("Twisty Puzzle - "+puzzle.getPuzzleName()).theme(theme).build();
+    titleWidget = TitleWidget.builder().title("Twisty Puzzle - " + puzzle.getPuzzleName())
+        .theme(theme).build();
+    moveHistoryWidget = RollingIconWidget.builder().width(WIDGET_WIDTH).height(WIDGET_HEIGHT)
+        .iconWidth(Move.MOVE_ICON_WIDTH).iconHeight(Move.MOVE_ICON_HEIGHT).theme(theme).build();
     /*frequencyGraph.setTitle("# Moves to solve");
     frequencyGraph.setXaxisHeader("# Moves");
     frequencyGraph.setYaxisHeader("Frequency");
     frequencyGraph.setGraphWidth(HISTORY_WIDTH * Move.MOVE_ICON_WIDTH);
     frequencyGraph.setGraphHeight(350);*/
+    // setup puzzle
+    initialisePuzzle();
   }
 
 
@@ -82,7 +92,7 @@ public final class TwistySimulation implements Simulation {
       scramblePuzzle();
     }
     moves = 0;
-    moveHistory.clear();
+    moveHistoryWidget.clearIcons();
     episodeID = UUID.randomUUID().toString();
   }
 
@@ -109,9 +119,10 @@ public final class TwistySimulation implements Simulation {
       initialisePuzzle();
     } else {
       // apply the move
+
       puzzle.applyMove(action.getMove());
       moves++;
-      moveHistory.add(action.getMove());
+  //    moveHistoryWidget.addIcon(action.getMove());
       if (puzzle.isSolved()) {
         // puzzle solved
         agent.send(TwistyResult.newBuilder().setState(puzzle.getState()).setSignal(TwistySignal.WIN)
@@ -135,29 +146,6 @@ public final class TwistySimulation implements Simulation {
     }
   }
 
- /* @Override
-  public void writeStatistics(File statisticsOutputFile) {
-    try {
-      PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(statisticsOutputFile)));
-      out.print("mean,");
-      out.println(frequencyGraph.getMean());
-      out.print("std,");
-      out.println(frequencyGraph.getStandardDeviation());
-      out.println("Values");
-      Iterator<Entry<Comparable<?>, Long>> iterator =
-          frequencyGraph.getFrequencyTable().entrySetIterator();
-      while (iterator.hasNext()) {
-        Entry<Comparable<?>, Long> e = iterator.next();
-        out.print(e.getKey());
-        out.print(",");
-        out.println(e.getValue());
-      }
-      out.close();
-    } catch (IOException e) {
-      log.error("Error writing stats to file " + statisticsOutputFile.getAbsolutePath(), e);
-    }
-  }*/
-
   private void scramblePuzzle() {
     for (int i = 0; i < SCRAMBLE_MOVES; i++) {
       try {
@@ -178,18 +166,18 @@ public final class TwistySimulation implements Simulation {
     // draw title
     graphics2D.drawImage(titleWidget.getImage(), 0, TOP_MARGIN, null);
     // draw puzzle
-    puzzle.drawPuzzle(graphics2D, LEFT_MARGIN, TOP_MARGIN+TITLE_HEIGHT+WIDGET_SPACING, theme);
+    puzzle.drawPuzzle(graphics2D, LEFT_MARGIN, TOP_MARGIN + TITLE_HEIGHT + WIDGET_SPACING, theme);
     // add logo
     graphics2D.drawImage(LOGO, HD_WIDTH - LOGO_WIDTH - RIGHT_MARGIN,
         (TOP_MARGIN + TITLE_HEIGHT + WIDGET_SPACING - LOGO_HEIGHT) / 2, null);
     // draw history
-    for (int i = 0; i < moveHistory.size(); i++) {
+/*    for (int i = 0; i < moveHistory.size(); i++) {
       BufferedImage moveImage = puzzle.getMoveImage(moveHistory.get(i));
       if (moveImage != null) {
         graphics2D.drawImage(moveImage, (i % HISTORY_WIDTH) * Move.MOVE_ICON_WIDTH + 1350,
             (i / HISTORY_WIDTH) * Move.MOVE_ICON_HEIGHT + 550, null);
       }
-    }
+    }*/
     //draw graphs
 /*    g.setColor(Color.BLACK);
     if (frequencyGraphImage != null) {
