d163bbb3ffc0406fa41e3d1141f8a860036e3acc
graham-evans <graham-evans@users.noreply.github.com>
2025-05-08 22:22:44 +0100

    update to ignore rotation and use integer scaling (removes rounding error at corners).

--- src/main/java/dev/aisandbox/server/simulation/twisty/model/shapes/Square.java
+++ src/main/java/dev/aisandbox/server/simulation/twisty/model/shapes/Square.java
@@ -3,7 +3,7 @@ package dev.aisandbox.server.simulation.twisty.model.shapes;
 import java.awt.Polygon;
 
 /**
- * A Unit square
+ * A Unit square, ignoring rotation.
  */
 public class Square implements CellShape {
 
@@ -11,12 +11,18 @@ public class Square implements CellShape {
   public Polygon getPolygon(int locationX, int locationY, int rotation, int scale) {
     Polygon poly = new Polygon();
     // length between center and corner
-    double len = Math.sqrt(2.0 * Math.pow(scale, 2.0));
+    //int len = (int) Math.sqrt(2.0 * Math.pow(scale, 2.0));
+    int len = scale;
     // add points
+    poly.addPoint(locationX-len,locationY-len);
+    poly.addPoint(locationX+len,locationY-len);
+    poly.addPoint(locationX+len,locationY+len);
+    poly.addPoint(locationX-len,locationY+len);
+    /*
     for (double ang = 45.0; ang < 360.0; ang += 90.0) {
       double a2 = Math.toRadians(ang + rotation);
       poly.addPoint((int) (locationX + len * Math.sin(a2)), (int) (locationY + len * Math.cos(a2)));
-    }
+    }*/
     return poly;
   }
 }
