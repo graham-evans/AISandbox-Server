bca8bfc8c8198f0171d83a7a0724ea25071f4a4f
graham-evans <graham-evans@users.noreply.github.com>
2025-05-08 22:01:35 +0100

    add middle layer and change shadings use new style arrows

--- src/main/java/dev/aisandbox/server/simulation/twisty/CuboidMoveIconBuilder.java
+++ src/main/java/dev/aisandbox/server/simulation/twisty/CuboidMoveIconBuilder.java
@@ -1,13 +1,13 @@
 package dev.aisandbox.server.simulation.twisty;
 
 import dev.aisandbox.server.engine.output.OutputConstants;
+import dev.aisandbox.server.engine.output.SpriteLoader;
 import dev.aisandbox.server.engine.widget.GraphicsUtils;
 import dev.aisandbox.server.simulation.twisty.model.Move;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import javax.imageio.ImageIO;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -17,25 +17,16 @@ public class CuboidMoveIconBuilder {
   final static int marginLeft = 10;
   final static int marginRight = 10;
   final static int marginTop = 10;
-  final static int marginBottom = 34;
+  final static int marginBottom = 10;
   // colours to fill squares
-  final static Color UNFILLED = Color.WHITE;
-  final static Color FILLED = Color.lightGray;
+  final static Color UNFILLED = Color.lightGray;
+  final static Color FILLED = Color.darkGray;
+  final static Color LINES = Color.BLACK;
   // arrow images
-  final static BufferedImage arrows;
+  final static List<BufferedImage> arrows = SpriteLoader.loadSpritesFromResources(
+      "/images/twisty/CuboidArrows.png", Move.MOVE_ICON_WIDTH, Move.MOVE_ICON_WIDTH);
 
-  static {
-    BufferedImage arrowImage = null;
-    try {
-      arrowImage = ImageIO.read(
-          CuboidMoveIconBuilder.class.getResourceAsStream("/images/twisty/CuboidArrows.png"));
-    } catch (IOException e) {
-      log.error("Error loading arrows!", e);
-    }
-    arrows = arrowImage;
-  }
-
-  // dimentions
+  // dimensions
   final int width;
   final int height;
   final int originX;
@@ -44,9 +35,12 @@ public class CuboidMoveIconBuilder {
   // drawing objects
   BufferedImage backgroundImage = GraphicsUtils.createBlankImage(Move.MOVE_ICON_WIDTH,
       Move.MOVE_ICON_HEIGHT, Color.WHITE);
+  BufferedImage middleImage = GraphicsUtils.createClearImage(Move.MOVE_ICON_WIDTH,
+      Move.MOVE_ICON_HEIGHT);
   BufferedImage foregroundImage = GraphicsUtils.createClearImage(Move.MOVE_ICON_WIDTH,
       Move.MOVE_ICON_HEIGHT);
   Graphics2D backgroundGraphics = backgroundImage.createGraphics();
+  Graphics2D middleGraphics = middleImage.createGraphics();
   Graphics2D foregroundGraphics = foregroundImage.createGraphics();
 
   public CuboidMoveIconBuilder(int width, int height, String name) {
@@ -58,53 +52,40 @@ public class CuboidMoveIconBuilder {
     double hScale = (Move.MOVE_ICON_WIDTH - marginLeft - marginRight) / (1.0 * width);
     double vScale = (Move.MOVE_ICON_HEIGHT - marginTop - marginBottom) / (1.0 * height);
     scale = Math.min(hScale, vScale);
-    // work out the origin of the grid
-//    originY = Move.MOVE_ICON_HEIGHT - marginBottom - (int) (
-//        (Move.MOVE_ICON_HEIGHT - marginBottom - marginTop - height * scale) / 2.0);
-
+    // center the grid
     originX = marginLeft + (int) ((Move.MOVE_ICON_WIDTH - marginLeft - marginRight - width * scale)
         / 2.0);
-
     // draw cuboid
-    drawCuboid(foregroundGraphics, originX, marginTop, width, height, scale);
+    drawCuboid(originX, marginTop, width, height, scale);
     // write label
     drawName(foregroundGraphics, name);
-//    midgroundGraphics.setColor(UNFILLED);
-//    midgroundGraphics.fillRect(originX, originY - (int) (height * scale), (int) (width * scale),
-//        (int) (height * scale));
-
-    //   midgroundGraphics.setColor(Color.cyan);
-    //   midgroundGraphics.fillRect(marginLeft,marginTop,Move
-    //   .MOVE_ICON_WIDTH-marginLeft-marginRight,Move.MOVE_ICON_HEIGHT-marginTop-marginBottom);
-    // load arrows
-    //  arrows = ImageIO.read(
-    //      CuboidMoveIcon.class.getResourceAsStream("/images/twisty/CuboidArrows.png"));
-    // overlay images onto background
   }
 
   public static CuboidMoveIconBuilder builer(int width, int height, String name) {
     return new CuboidMoveIconBuilder(width, height, name);
   }
 
-  private static void drawCuboid(Graphics2D g, int originX, int originY, int width, int height,
-      double scale) {
-    g.setColor(Color.darkGray);
+  private static void drawName(Graphics2D g, String name) {
+    GraphicsUtils.drawCenteredText(g, 0,
+        Move.MOVE_ICON_HEIGHT - OutputConstants.LOG_FONT_HEIGHT - marginBottom,
+        Move.MOVE_ICON_WIDTH, OutputConstants.LOG_FONT_HEIGHT, name, OutputConstants.LOG_FONT,
+        Color.BLACK);
+  }
+
+  private void drawCuboid(int originX, int originY, int width, int height, double scale) {
+    middleGraphics.setColor(UNFILLED);
+    middleGraphics.fillRect(originX, originY, (int) (width * scale), (int) (height * scale));
+    foregroundGraphics.setColor(LINES);
     // draw front vertical lines
     for (int i = 0; i <= width; i++) {
-      g.drawLine((int) (originX + i * scale), (int) originY, (int) (originX + i * scale),
-          (int) (originY + height * scale));
+      foregroundGraphics.drawLine((int) (originX + i * scale), (int) originY,
+          (int) (originX + i * scale), (int) (originY + height * scale));
     }
     // draw front horizontal lines
     for (int i = 0; i <= height; i++) {
-      g.drawLine(originX, originY + (int) (i * scale), originX + (int) (width * scale),
-          originY + (int) (i * scale));
+      foregroundGraphics.drawLine(originX, originY + (int) (i * scale),
+          originX + (int) (width * scale), originY + (int) (i * scale));
     }
-  }
-
-  private static void drawName(Graphics2D g, String name) {
-    GraphicsUtils.drawCenteredText(g, 0,
-        Move.MOVE_ICON_HEIGHT - OutputConstants.LOG_FONT_HEIGHT * 2, Move.MOVE_ICON_WIDTH,
-        OutputConstants.LOG_FONT_HEIGHT, name, OutputConstants.LOG_FONT, Color.BLACK);
   }
 
   public CuboidMoveIconBuilder fillFrontFace() {
@@ -128,7 +109,7 @@ public class CuboidMoveIconBuilder {
   public CuboidMoveIconBuilder fillFromBottom(int n) {
     for (int x = 0; x < width; x++) {
       for (int y = 0; y < n; y++) {
-        fillFrontFace(x, height - y-1);
+        fillFrontFace(x, height - y - 1);
       }
     }
     return this;
@@ -146,24 +127,43 @@ public class CuboidMoveIconBuilder {
   public CuboidMoveIconBuilder fillFromRight(int n) {
     for (int x = 0; x < n; x++) {
       for (int y = 0; y < height; y++) {
-        fillFrontFace(width-x-1, y);
+        fillFrontFace(width - x - 1, y);
       }
     }
     return this;
   }
+
   private void fillFrontFace(int x, int y) {
     fillFrontFace(x, y, FILLED);
   }
 
   private void fillFrontFace(int x, int y, Color color) {
-    backgroundGraphics.setColor(color);
-    backgroundGraphics.fillRect((int) (originX + x * scale), (int) (marginTop + scale * y),
+    middleGraphics.setColor(color);
+    middleGraphics.fillRect((int) (originX + x * scale), (int) (marginTop + scale * y),
         (int) scale + 1, (int) scale + 1);
   }
 
   public CuboidMoveIconBuilder setRotation(char face, boolean inverse) {
     switch (face) {
+      case 'F':
+        foregroundGraphics.drawImage(arrows.get(inverse ? 5 : 0), 0, 0, null);
+        break;
+      case 'B':
+        backgroundGraphics.drawImage(arrows.get(inverse ? 0 : 5), 0, 0, null);
+        break;
+      case 'U':
+        foregroundGraphics.drawImage(arrows.get(inverse ? 6 : 1), 0, 0, null);
+        break;
+      case 'D':
+        foregroundGraphics.drawImage(arrows.get(inverse ? 7 : 2), 0, 0, null);
+        break;
       case 'R':
+        foregroundGraphics.drawImage(arrows.get(inverse ? 8 : 3), 0, 0, null);
+        break;
+      case 'L':
+        foregroundGraphics.drawImage(arrows.get(inverse ? 9 : 4), 0, 0, null);
+        break;
+ /*     case 'R':
         backgroundGraphics.drawImage(arrows.getSubimage(32 + 4 * 60, 20, 28, 40), 32, 20, null);
         foregroundGraphics.drawImage(arrows.getSubimage(40 + (inverse ? 60 : 0), 20, 20, 40), 40,
             20, null);
@@ -190,6 +190,8 @@ public class CuboidMoveIconBuilder {
         backgroundGraphics.drawImage(arrows.getSubimage((inverse ? 2 : 3) * 60, 0, 60, 100), 0, 0,
             null);
         break;
+
+  */
     }
     return this;
   }
@@ -200,6 +202,7 @@ public class CuboidMoveIconBuilder {
         BufferedImage.TYPE_INT_RGB);
     Graphics2D g = image.createGraphics();
     g.drawImage(backgroundImage, 0, 0, null);
+    g.drawImage(middleImage, 0, 0, null);
     g.drawImage(foregroundImage, 0, 0, null);
     return image;
   }
