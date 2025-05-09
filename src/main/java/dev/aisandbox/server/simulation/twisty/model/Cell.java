55829d12445cf00eb9bbe18d5698a7c46c33b35c
graham-evans <graham-evans@users.noreply.github.com>
2025-05-09 22:35:39 +0100

    add javadoc

--- src/main/java/dev/aisandbox/server/simulation/twisty/model/Cell.java
+++ src/main/java/dev/aisandbox/server/simulation/twisty/model/Cell.java
@@ -7,24 +7,44 @@ import lombok.Setter;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
+/**
+ * Represents a cell within the twisty simulation model.
+ * A cell contains information about its shape, size, position, and color.
+ * It is used to represent individual elements in the twisty puzzle simulation.
+ */
 public class Cell {
 
+  /** The shape of the cell, defined as a ShapeEnum type. */
   @Getter
   @Setter
   ShapeEnum shape;
+  
+  /** The scale factor that determines the size of the cell. */
   @Getter
   @Setter
   int scale;
+  
+  /** The x-coordinate of the cell's position. */
   @Getter
   @Setter
   int locationX;
+  
+  /** The y-coordinate of the cell's position. */
   @Getter
   @Setter
   int locationY;
+  
+  /** The color of the cell, defined as a ColourEnum type. */
   @Getter
   @Setter
   ColourEnum colour;
 
+  /**
+   * Gets the polygon representation of this cell.
+   * The polygon is positioned at the cell's location coordinates and sized according to its scale.
+   *
+   * @return The Polygon representation of the cell, or null if no shape is defined
+   */
   public Polygon getPolygon() {
     if (shape == null) {
       return null;
@@ -33,6 +53,12 @@ public class Cell {
     }
   }
 
+  /**
+   * Returns a string representation of this Cell.
+   * Includes information about the cell's shape, scale, and location.
+   *
+   * @return A string representation of the Cell
+   */
   @Override
   public String toString() {
     return new ToStringBuilder(this, ToStringStyle.SIMPLE_STYLE).append("shape", shape)
--- src/main/java/dev/aisandbox/server/simulation/twisty/model/CompiledMove.java
+++ src/main/java/dev/aisandbox/server/simulation/twisty/model/CompiledMove.java
@@ -4,38 +4,91 @@ import java.awt.image.BufferedImage;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Represents a compiled move for a twisty puzzle.
+ * <p>
+ * This class defines a move as a permutation matrix that can be applied to puzzle states.
+ * Each move transforms the puzzle state by rearranging elements according to the defined matrix.
+ * The moves can be visualized with an associated image and have an assigned cost.
+ * </p>
+ */
 public class CompiledMove {
 
+  /** The number of elements in the puzzle state that this move affects. */
   private final int cardinality;
 
+  /** 
+   * The permutation matrix representing this move.
+   * <p>
+   * Each value matrix[i] = j means that the element at position i in the new state
+   * should be the element that was at position j in the original state.
+   * </p>
+   */
   private final int[] matrix;
 
+  /** The image representing this move for visualization purposes. */
   @Getter
   @Setter
   BufferedImage image;
+  
+  /** The cost associated with performing this move. */
   @Getter
   @Setter
   int cost;
 
+  /**
+   * Constructs a new CompiledMove with the specified cardinality.
+   *
+   * @param cardinality the number of elements in the puzzle state
+   */
   public CompiledMove(int cardinality) {
     this.cardinality = cardinality;
     matrix = new int[cardinality];
   }
 
+  /**
+   * Resets the move to the identity permutation.
+   * <p>
+   * After calling this method, applying the move will not change the state.
+   * </p>
+   */
   protected void resetMove() {
     for (int i = 0; i < cardinality; i++) {
       matrix[i] = i;
     }
   }
 
+  /**
+   * Sets a specific element in the permutation matrix.
+   *
+   * @param index the position in the matrix to set
+   * @param value the new value for that position
+   */
   protected void setMatrixElement(int index, int value) {
     matrix[index] = value;
   }
 
+  /**
+   * Gets a specific element from the permutation matrix.
+   *
+   * @param index the position in the matrix to query
+   * @return the value at the specified position
+   */
   protected int getMatrixElement(int index) {
     return matrix[index];
   }
 
+  /**
+   * Applies this move to a puzzle state.
+   * <p>
+   * The state is represented as a string, where each character represents
+   * a specific element of the puzzle. The move rearranges these elements
+   * according to the permutation matrix.
+   * </p>
+   *
+   * @param state the current puzzle state as a string
+   * @return the new puzzle state after applying this move
+   */
   public String applyMove(String state) {
     StringBuilder sb = new StringBuilder();
     for (int i = 0; i < cardinality; i++) {
