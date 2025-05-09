01155c47e2efb838d59ec1e77513edf51c1dd2d9
graham-evans <graham-evans@users.noreply.github.com>
2025-05-09 22:33:21 +0100

    remove unused caching

--- src/main/java/dev/aisandbox/server/engine/widget/RollingIconWidget.java
+++ src/main/java/dev/aisandbox/server/engine/widget/RollingIconWidget.java
@@ -1,42 +1,51 @@
 package dev.aisandbox.server.engine.widget;
 
+import static dev.aisandbox.server.engine.output.OutputConstants.WIDGET_TITLE_FONT;
+import static dev.aisandbox.server.engine.output.OutputConstants.WIDGET_TITLE_HEIGHT;
+
 import dev.aisandbox.server.engine.Theme;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.NonNull;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class RollingIconWidget {
-
+  private final static int PADDING = 16;
   private final int width;
   private final int height;
   private final int iconWidth;
   private final int iconHeight;
-  private final int iconPadding;
   private final int maxXCount;
   private final int maxMemory;
   private final boolean useCache;
   private final String title;
   private final Theme theme;
   private final List<BufferedImage> images = new ArrayList<>();
+  private final int iconOriginX;
+  private final int iconOriginY;
 
   private BufferedImage cachedImage = null;
 
-  public RollingIconWidget(int width, int height, int iconWidth, int iconHeight, int iconPadding,
-      boolean useCache, String title, Theme theme) {
+  public RollingIconWidget(int width, int height, int iconWidth, int iconHeight, boolean useCache,
+      String title, Theme theme) {
     this.width = width;
     this.height = height;
     this.iconWidth = iconWidth;
     this.iconHeight = iconHeight;
-    this.iconPadding = iconPadding;
     this.useCache = useCache;
     this.title = title;
     this.theme = theme;
-    this.maxXCount = width / (iconWidth + iconPadding);
-    int maxYCount = height / (iconHeight + iconPadding);
+    this.maxXCount = (width-PADDING*2) / iconWidth;
+    int maxYCount = (height - WIDGET_TITLE_HEIGHT-PADDING*3) / iconHeight;
     this.maxMemory = maxXCount * maxYCount;
+    log.debug("Setting up wulling icon widget with max number of icons = {}", maxMemory);
+    iconOriginX = (width -PADDING*2- iconWidth * maxXCount) / 2+PADDING;
+    iconOriginY = (height - WIDGET_TITLE_HEIGHT -PADDING*3- iconHeight * maxYCount) / 2 + WIDGET_TITLE_HEIGHT+PADDING*2;
   }
 
   public static RollingIconWidgetBuilder builder() {
@@ -44,16 +53,18 @@ public class RollingIconWidget {
   }
 
   public void clearIcons() {
+    log.info("Clear icons");
     images.clear();
     cachedImage = null;
   }
 
-  public void addIcon(BufferedImage image) {
-    assert (image.getWidth() == width && image.getHeight() == height);
-    images.add(image);
+  public void addIcon(@NonNull BufferedImage iconImage) {
+    assert (iconImage.getWidth() == iconWidth && iconImage.getHeight() == iconHeight);
+    images.add(iconImage);
     while (images.size() > maxMemory) {
       images.removeFirst();
     }
+    log.info("Adding icon: {} / {}", images.size(), maxMemory);
     cachedImage = null;
   }
 
@@ -66,36 +77,27 @@ public class RollingIconWidget {
     BufferedImage image = cachedImage;
     if (image == null) {
       image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-      Graphics2D g = image.createGraphics();
-      drawImage(g, 0, 0);
+      Graphics2D graphics2D = image.createGraphics();
+      GraphicsUtils.setupRenderingHints(graphics2D);
+      graphics2D.setColor(theme.getWidgetBackground());
+      graphics2D.fillRect(0, 0, width, height);
+      graphics2D.setColor(theme.getText());
+      GraphicsUtils.drawCenteredText(graphics2D, 0, PADDING, width, WIDGET_TITLE_HEIGHT, title,
+          WIDGET_TITLE_FONT, theme.getText());
+      log.info("Drawing image: {} / {}", images.size(), maxMemory);
+      for (int i = 0; i < images.size(); i++) {
+        BufferedImage icon = images.get(i);
+        // work out position index
+        int dx = i % maxXCount;
+        int dy = i / maxXCount;
+        // draw the image
+        graphics2D.drawImage(icon, iconOriginX + dx * iconWidth, iconOriginY + dy * iconHeight, null);
+      }
       if (useCache) {
         cachedImage = image;
       }
     }
     return image;
-  }
-
-  /**
-   * Draw the icons on an already existing Graphics2D object.
-   * <p>
-   * This will not use any cache.
-   *
-   * @param graphics2D The graphics context to draw with
-   * @param originX    The horizontal origin
-   * @param originY    The vertical origin
-   */
-  public void drawImage(Graphics2D graphics2D, int originX, int originY) {
-    graphics2D.setColor(theme.getWidgetBackground());
-    graphics2D.fillRect(originX, originY, width, height);
-    for (int i = 0; i < images.size(); i++) {
-      BufferedImage image = images.get(i);
-      // work out position index
-      int dx = i % maxXCount;
-      int dy = i / maxXCount;
-      // draw the image
-      graphics2D.drawImage(image, originX + dx * iconWidth, originY + dy + iconHeight, null);
-    }
-
   }
 
   @Setter
@@ -106,14 +108,12 @@ public class RollingIconWidget {
     private int height = 200;
     private int iconWidth = 50;
     private int iconHeight = 50;
-    private int iconPadding = 0;
     private boolean useCache = false;
     private String title = "Recent Icons";
     private Theme theme = Theme.LIGHT;
 
     public RollingIconWidget build() {
-      return new RollingIconWidget(width, height, iconWidth, iconHeight, iconPadding, useCache,
-          title, theme);
+      return new RollingIconWidget(width, height, iconWidth, iconHeight, useCache, title, theme);
     }
   }
 
