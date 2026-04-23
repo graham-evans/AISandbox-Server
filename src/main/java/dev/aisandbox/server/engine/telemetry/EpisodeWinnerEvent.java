b0b8d85a793c07350431f5f39982379445f94989
Graham Evans <2950775+graham-evans@users.noreply.github.com>
2026-04-23 23:34:09 +0100

    correct copyright notice

--- src/main/java/dev/aisandbox/server/engine/telemetry/EpisodeWinnerEvent.java
+++ src/main/java/dev/aisandbox/server/engine/telemetry/EpisodeWinnerEvent.java
@@ -1,10 +1,8 @@
 /*
- *
- *  * AI Sandbox - This program is free software: you can redistribute it and/or modify it under the
- *  * terms of version 3 of the GNU General Public License. See the README and LICENCE files for
- *  * more information.
- *
- */
+ * AI Sandbox - This program is free software: you can redistribute it and/or modify it under the
+ * terms of version 3 of the GNU General Public License. See the README and LICENCE files for
+ * more information.
+*/
 
 package dev.aisandbox.server.engine.telemetry;
 
