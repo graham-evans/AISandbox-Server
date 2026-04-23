0553505a85e721f34c6c423f6f1fbf03685a4321
Graham Evans <2950775+graham-evans@users.noreply.github.com>
2026-04-23 20:45:27 +0100

    base telemetry classes

---
+++ src/main/java/dev/aisandbox/server/engine/telemetry/TelemetryEngine.java
@@ -0,0 +1,16 @@
+/*
+ *
+ * AI Sandbox - This program is free software: you can redistribute it and/or modify it under the
+ * terms of version 3 of the GNU General Public License. See the README and LICENCE files for
+ * more information.
+ *
+ */
+
+package dev.aisandbox.server.engine.telemetry;
+
+public interface TelemetryEngine {
+
+    void initialise();
+    void writeTelementryEvent(TelemetryEvent event);
+    void close();
+}
